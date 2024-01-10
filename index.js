60bfb2cd093a6440b1148faee4c52f8fd819cf2c
jason-tung <jasontungnyc@gmail.com>
2024-01-10 18:06:57 -0500

    post my latest commit

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ import { exec } from 'child_process';
 import fetch from 'node-fetch';
 import os from 'os';
 import path from 'path';
-import {dirname} from 'path'
-import fs from 'fs'
+import { dirname } from 'path';
+import fs from 'fs';
 import 'dotenv/config';
 
 const getCurrentTime = () => {
-    const currentDate = new Date(new Date().toLocaleString("en-US", {timeZone: "America/New_York"}));
+    const currentDate = new Date(
+        new Date().toLocaleString('en-US', { timeZone: 'America/New_York' })
+    );
 
     const year = currentDate.getFullYear().toString().padStart(4, '0');
     const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
@@ -18,8 +20,12 @@ const getCurrentTime = () => {
     const hours = currentDate.getHours().toString().padStart(2, '0');
     const minutes = currentDate.getMinutes().toString().padStart(2, '0');
 
-    return`${month}/${day}/${year} ${hours}:${minutes}`;
-}
+    return `${month}/${day}/${year} ${hours}:${minutes}`;
+};
+
+const extractLatestCommit = (s) => {
+    return s.match(/(?<=[ \t]+[a-z0-9]+\.\.)[a-z0-9]+/)[0];
+};
 
 const app = express();
 
@@ -27,7 +33,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const homeDir = os.homedir();
-const resourceDir = path.join(homeDir, 'leetcode-server', 'public')
+const resourceDir = path.join(homeDir, 'leetcode-server', 'public');
 
 app.use('/public', express.static('public')); // works
 
@@ -43,104 +49,122 @@ const authenticate = (req, res, next) => {
 };
 
 const getRandomImage = (key) => {
-  const files = fs.readdirSync(resourceDir);
-  key = key && key < 1 ? key : Math.random();
-  const randomIndex = Math.floor(key * files.length);
-  return files[randomIndex];
+    const files = fs.readdirSync(resourceDir);
+    key = key && key < 1 ? key : Math.random();
+    const randomIndex = Math.floor(key * files.length);
+    return files[randomIndex];
 };
 app.get('/randomimage', (req, res) => {
-  const randomImage = getRandomImage(req.query.key);
-  const imagePath = path.join(resourceDir, randomImage);
-  
-  res.sendFile(imagePath);
+    const randomImage = getRandomImage(req.query.key);
+    const imagePath = path.join(resourceDir, randomImage);
+
+    res.sendFile(imagePath);
 });
 
-app.get('/images/:item', (req, res) => {
-  if (req.params.item == "punch"){
-    fs.readFile(path.join(resourceDir, 'punch.png'), (err, data) => {
-      if (err) {
-        res.status(500).send('Internal Server Error');
-        return;
-      }
-  
-      res.writeHead(200, { 'Content-Type': 'image/jpeg' });
-      res.end(data);
-    })
-  }
-})
-
-app.get('/', (req, res) => res.send('hi')); 
+app.get('/', (req, res) => res.send('hi'));
 
 app.post('/updateGithub', authenticate, (req, res) => {
     console.log(req.body);
-    const {difficulty, formattedTitle, fileText} = req.body
-    const filePath = path.join(homeDir, 'leetcode', difficulty, `${formattedTitle}.py`);
-    fs.writeFile(filePath, fileText,  (err) => {
+    const { difficulty, formattedTitle, fileText } = req.body;
+    const filePath = path.join(
+        homeDir,
+        'leetcode',
+        difficulty,
+        `${formattedTitle}.py`
+    );
+    fs.writeFile(filePath, fileText, (err) => {
         if (err) {
-          console.error('Error writing file:', err);
+            console.error('Error writing file:', err);
         } else {
-          console.log('File written successfully.');
-          const options = {
-            cwd: path.join(homeDir, 'leetcode')
-        }
-        exec('git add .', options, (error, stdout, stderr)=>{
-            if (error || stderr){
-                console.error("uh oh stinky 1", error, stderr )
-                res.status(500).send('Error executing command');
-            }
-            else{
-                const commitMessage = `[jasbob-leetcode-bot] automated upload of <${difficulty}> ${formattedTitle}`       
-                // const commitMessage = `[jasbob-leetcode-bot] what the flip ?`                  
-                exec(`git commit -m '${commitMessage}'`, options, (error, stdout, stderr)=>{
-                    if (error || stderr){
-                        console.error("uh oh stinky 2", stdout, error, stderr )
-                        res.status(500).send('Error executing command');
-                    }
-                    else{
-                        exec("git push", options, (error, stdout, stderr)=>{
-                            if (error ){
-                                // push writes to stderror 
-                                console.error("uh oh stinky 3", error )
+            console.log('File written successfully.');
+            const options = {
+                cwd: path.join(homeDir, 'leetcode'),
+            };
+            exec('git add .', options, (error, stdout, stderr) => {
+                if (error || stderr) {
+                    console.error('uh oh stinky 1', error, stderr);
+                    res.status(500).send('Error executing command');
+                } else {
+                    const commitMessage = `[jasbob-leetcode-bot] automated upload of <${difficulty}> ${formattedTitle}`;
+                    // const commitMessage = `[jasbob-leetcode-bot] what the flip ?`
+                    exec(
+                        `git commit -m '${commitMessage}'`,
+                        options,
+                        (error, stdout, stderr) => {
+                            if (error || stderr) {
+                                console.error(
+                                    'uh oh stinky 2',
+                                    stdout,
+                                    error,
+                                    stderr
+                                );
                                 res.status(500).send('Error executing command');
-                            }
-                            else{
-                                const data = {
-                                    "embeds": [
-                                      {
-                                        "fields": [
-                                          {
-                                            "name": `[jasbob-leetcode-bot] Automated Upload Triggered!`,
-                                            "value": `Uploaded <${difficulty}> ${formattedTitle} [(here)](https://github.com/jason-tung/leetcode)`
-                                          }
-                                        ],
-                                        "thumbnail": {
-                                          "url": `http://jasontung.me:3001/randomimage?key=${Math.random()}`
-                                        },
-                                        "footer": {
-                                          "text": `powered by jasbob-bot ・ ${getCurrentTime()}`,
-                                          "icon_url": "https://avatars.githubusercontent.com/u/153464167?v=4"
+                            } else {
+                                exec(
+                                    'git push',
+                                    options,
+                                    (error, stdout, stderr) => {
+                                        if (error) {
+                                            // push writes to stderror
+                                            console.error(
+                                                'uh oh stinky 3',
+                                                error
+                                            );
+                                            res.status(500).send(
+                                                'Error executing command'
+                                            );
+                                        } else if (
+                                            stdout.includes('github.com')
+                                        ) {
+                                            const commit =
+                                                extractLatestCommit(stdout);
+                                            const data = {
+                                                embeds: [
+                                                    {
+                                                        fields: [
+                                                            {
+                                                                name: `[jasbob-leetcode-bot] Automated Upload Triggered!`,
+                                                                value: `Uploaded <${difficulty}> ${formattedTitle} [(here)](https://github.com/jason-tung/leetcode/commit/${commit})`,
+                                                            },
+                                                        ],
+                                                        thumbnail: {
+                                                            url: `http://jasontung.me:3001/randomimage?key=${Math.random()}`,
+                                                        },
+                                                        footer: {
+                                                            text: `powered by jasbob-bot ・ ${getCurrentTime()}`,
+                                                            icon_url:
+                                                                'https://avatars.githubusercontent.com/u/153464167?v=4',
+                                                        },
+                                                    },
+                                                ],
+                                            };
+                                            fetch(process.env.WEBHOOK, {
+                                                method: 'POST',
+                                                headers: {
+                                                    'Content-Type':
+                                                        'application/json',
+                                                },
+                                                body: JSON.stringify(data),
+                                            });
+                                            console.log('omg we uploaded!?!');
+                                            res.status(200).send(
+                                                `${formattedTitle}`
+                                            );
+                                        } else {
+                                            console.log(
+                                                "hmm something bad happened! here's log of stdout and stderr"
+                                            );
+                                            console.log(stdout, stderr);
                                         }
-                                      }
-                                    ]
-                                  }
-                                fetch(process.env.WEBHOOK, {
-                                    method: 'POST',
-                                    headers: {
-                                      'Content-Type': 'application/json'
-                                    },
-                                    body: JSON.stringify(data)
-                                  })
-                                console.log("omg we uploaded!?!")
-                                res.status(200).send(`${formattedTitle}`)
+                                    }
+                                );
                             }
-                        })
-                    }
-                })
-            }
-        })
+                        }
+                    );
+                }
+            });
         }
-      });
-
+    });
 });
 
 const PORT = 3001;
--- test.js
+++ test.js
@@ -1,14 +1,33 @@
 import os from 'os';
 import path from 'path';
-import {dirname} from 'path'
-import fs from 'fs'
+import { dirname } from 'path';
+import fs from 'fs';
+import { exec } from 'child_process';
 
 const homeDir = os.homedir();
-const resourceDir = path.join(homeDir, 'leetcode-server', 'public')
+const resourceDir = path.join(homeDir, 'leetcode-server', 'public');
+const options = {
+    cwd: path.join(homeDir, 'projects', 'leetcode'),
+};
+exec(`ls`, options, (error, stdout, stderr) => {
+    if (error || stderr) {
+        console.error('uh oh stinky 2', stdout, error, stderr);
+    }
+    console.log(stdout);
+});
 
+const extractLatestCommit = (s) => {
+    return s.match(/(?<=[ \t]+[a-z0-9]+\.\.)[a-z0-9]+/)[0];
+};
 
-fs.readdir(resourceDir, (err, files) => {
-files.forEach(file => {
-console.log(file)
-})
-})
+const sample = `Enumerating objects: 5, done.
+Counting objects: 100% (5/5), done.
+Delta compression using up to 8 threads
+Compressing objects: 100% (3/3), done.
+Writing objects: 100% (3/3), 296 bytes | 296.00 KiB/s, done.
+Total 3 (delta 2), reused 0 (delta 0), pack-reused 0
+remote: Resolving deltas: 100% (2/2), completed with 2 local objects.
+To github.com:jason-tung/leetcode-archiver.git
+   b638a63..5bdec10  main -> main`;
+
+console.log(extractLatestCommit(sample));
